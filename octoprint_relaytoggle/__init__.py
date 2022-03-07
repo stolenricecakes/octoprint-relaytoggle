250ef406b6becd94fc2eaa39998ae162876c7a6c
awall <rewfus@gmail.com>
2022-03-06 20:55:27 -0600

    uncommented all the GPIO stuff

--- octoprint_relaytoggle/__init__.py
+++ octoprint_relaytoggle/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
 
-#import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 class RelaytogglePlugin(octoprint.plugin.StartupPlugin,
                         octoprint.plugin.TemplatePlugin,
@@ -14,8 +14,8 @@ class RelaytogglePlugin(octoprint.plugin.StartupPlugin,
     def on_after_startup(self):
         port = int(self._settings.get(["port"]))
         self._logger.info("HOLY TINKLES Im starting******* on port: %d" % port)
-#        GPIO.setmode(GPIO.BCM)
-#        GPIO.setup(17, GPIO.OUT)
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(port, GPIO.OUT)
         self._logger.info("setting up GPIO to be legit output")
 
     def get_settings_defaults(self):
@@ -23,26 +23,22 @@ class RelaytogglePlugin(octoprint.plugin.StartupPlugin,
 
     def get_template_configs(self):
         return [
-            dict(type="navbar", custom_bindings=False),
             dict(type="settings", custom_bindings=False)
         ]
 
     def on_event(self,event,payload):
         if event == 'PrintStarted':
-#            GPIO.output(17, GPIO.HIGH)
+            GPIO.output(port, GPIO.HIGH)
             self._logger.info("print started, lights on")
         elif event == 'PrintFailed':
-#            GPIO.output(17, GPIO.LOW)
+            GPIO.output(port, GPIO.LOW)
             self._logger.info("print failed, lights off")
         elif event == 'PrintDone':
-#            GPIO.output(17, GPIO.LOW)
+            GPIO.output(port, GPIO.LOW)
             self._logger.info("print done, lights off")
         elif event == 'PrintCancelled':
-#            GPIO.output(17, GPIO.LOW)
+            GPIO.output(port, GPIO.LOW)
             self._logger.info("print cancelled. lights off")
-
-
-
 
 
 __plugin_name__ = "Relaytoggle"
